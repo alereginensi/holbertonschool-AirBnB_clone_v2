e119a77b3dd9a159ef52a1adaa0482a83beec181
alereginensi <reginensia@gmail.com>
2022-08-25 16:12:46 -0700

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ echo "
    Holberton School (body)
  </body>
  </html>" > sudo /data/web_static/releases/test/index.html
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
