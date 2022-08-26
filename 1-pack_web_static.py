dc190e842ec5836cc027c718f266193528bda6f5
alereginensi <reginensia@gmail.com>
2022-08-25 17:03:21 -0700

    pep8 fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,16 +4,15 @@
 # Clone repo, using the function do_pack.
 
 from fabric.api import local
-from datetime import datetime
+from time import strftime
 
 
 def do_pack():
     """
-    Script that generates a .tgz archive from the contents of the web_static
-    folder
+    Script that generates a .tgz archive from the contents of
+    the web_static folder
     """
-    name = "web_static_" +\
-            datetime.strftime(datetime.now(), "%Y%m%d%H%M%S") + ".tgz"
+    name = "versions/web_static_{}.tgz".format(time)
     local("mkdir -p versions")
     try:
         local("tar -czvf ./versions/{} ./web_static" .format(name))
