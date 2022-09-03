716084b052c2ad4f5bb5a5cfa2c444be13c1d098
alereginensi <reginensia@gmail.com>
2022-09-02 18:53:04 -0700

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# script that starts a Flask web application:
+# Your web application must be listening on 0.0.0.0, port 5000
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''starts a Flask web application'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    '''/hbnb: display “HBNB”'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    '''/c/<text>: display “C ” followed by the value of the text variable'''
+    parce_text = escape(text.replace('_', ' '))
+    return f'C {parce_text}'
+
+
+@app.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def text_2(text):
+    '''display “Python ”, followed by the value of the text variable'''
+    parce_text = escape(text.replace('_', ' '))
+    return f'Python {parce_text}'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
